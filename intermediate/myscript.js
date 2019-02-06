c0dba19fdbd66176c05209f451e35bbff9b882c4
Anesto27 <anes.2503@gmx.at>
2019-02-06 14:04:22 +0100

    add a comment

--- intermediate/myscript.js
+++ intermediate/myscript.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var color=['red','green','blue'];
 	var classes=['.notebook_image','.phone_image','.tablet_image'];
 	for(i=0; i<color.length; i++){
-	$(classes[i]).css('background-color',color[i]);
+	$(classes[i]).css('background-color',color[i]);//first solution
 	}
 	//$('.phone_image').css('background-color','green');
 	//$('.tablet_image').css('background-color','blue');
